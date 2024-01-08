1c46c0f0a1f278386e32453876a65d8ccf220159
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-08 10:58:03 +0930

    Update q1_task4.py

--- q1_task4.py
+++ q1_task4.py
@@ -1,1 +1,9 @@
 currently working on this
+
+import spacy
+from spacy import displacy 
+from transformers import BertTokenizer, BertForTokenClassification
+
+#Establisng spacy models
+nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
+nlp_sci_sm=spacy.load('en_core_sci_sm')
