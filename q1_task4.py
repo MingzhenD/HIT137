2609213779300910c33cc7f156bc4c96bc70eb12
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-08 14:49:20 +0930

    Update q1_task4.py

--- q1_task4.py
+++ q1_task4.py
@@ -4,6 +4,10 @@ import spacy
 from spacy import displacy 
 from transformers import BertTokenizer, BertForTokenClassification
 
-#Establisng spacy models
+# Establisng spacy models
 nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
 nlp_sci_sm=spacy.load('en_core_sci_sm')
+
+# Setting BioBERT pretrained functions
+model=BertForTokenClassification.from_pretrained("monologg/biobert_v1.1_pubmed")
+tokenizer=BertTokenizer.from_pretrained("monologg/biobert_v1.1_pubmed")
