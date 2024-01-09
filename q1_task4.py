a5738fee13d7311afe811781666bc9d2f10bb5e9
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-09 13:39:19 +0930

    Update q1_task4.py

--- q1_task4.py
+++ q1_task4.py
@@ -3,6 +3,14 @@ currently working on this
 import spacy
 from spacy import displacy 
 from transformers import BertTokenizer, BertForTokenClassification
+import pandas as pd
+
+class NERProcessor:
+  def __init__(self, spacy_model, bio_model):
+    self.nlp_sci_sm=spacy_model
+    self.nlp_bc5cdr_md=spacy.load(bio_model)
+    self.tokenizer=BertTokenizer.from_pretrained(bio_model)
+    self.model=BertForTokenClassification.from_pretrained(bio_model)
 
 # Establisng spacy models
 nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
@@ -19,3 +27,13 @@ def extract_entities_spacy(txt,design):
   return entities
 
 #Entity extraction(BioBERT)
+def extract_entities_biobert(self,txt):
+  input=self.tokenizer(txt,return_tensors="pt")
+  output=self.model(**input)
+  predictions=output.logits.argmax(dim=2)
+  
+# Predictions into entities
+entities=[(self.tokenizer.convert_ids_to_tokens(input['input_ids'][0][i].item()),
+           self.model.config.id2label[predictions[0][i].item()])
+          for i in range(len(predictions[0]))]
+return entities
