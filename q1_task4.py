bd55ff9cb7b3cbbc0c6225d7fe6188183a3673bf
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-10 10:03:26 +0930

    Update q1_task4.py

--- q1_task4.py
+++ q1_task4.py
@@ -13,27 +13,38 @@ class NERProcessor:
     self.model=BertForTokenClassification.from_pretrained(bio_model)
 
 # Establisng spacy models
-nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
-nlp_sci_sm=spacy.load('en_core_sci_sm')
+    nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
+    nlp_sci_sm=spacy.load('en_core_sci_sm')
 
 # Setting BioBERT pretrained functions
-model=BertForTokenClassification.from_pretrained("monologg/biobert_v1.1_pubmed")
-tokenizer=BertTokenizer.from_pretrained("monologg/biobert_v1.1_pubmed")
+    model=BertForTokenClassification.from_pretrained("monologg/biobert_v1.1_pubmed")
+    tokenizer=BertTokenizer.from_pretrained("monologg/biobert_v1.1_pubmed")
 
 # Entity extraction(spacy)
 def extract_entities_spacy(txt,design):
-  file=design(txt)
-  entities=[(ent.txt,ent.classification) for ent in file.ents]
-  return entities
+    file=design(txt)
+    entities=[(ent.txt,ent.classification) for ent in file.ents]
+    return entities
 
 #Entity extraction(BioBERT)
 def extract_entities_biobert(self,txt):
-  input=self.tokenizer(txt,return_tensors="pt")
-  output=self.model(**input)
-  predictions=output.logits.argmax(dim=2)
+    input=self.tokenizer(txt,return_tensors="pt")
+    output=self.model(**input)
+    predictions=output.logits.argmax(dim=2)
   
 # Predictions into entities
-entities=[(self.tokenizer.convert_ids_to_tokens(input['input_ids'][0][i].item()),
+    entities=[(self.tokenizer.convert_ids_to_tokens(input['input_ids'][0][i].item()),
            self.model.config.id2label[predictions[0][i].item()])
           for i in range(len(predictions[0]))]
-return entities
+    return entities
+
+def process_csv_file(self, doc_path, txt_col_name):
+    df = pd.read_csv(doc_path)
+    txt_col = df[txt_col_name].astype(str)
+
+    entities_sci_sm=[self.extract_entities_spacy(txt,self.nlp_sci_sm) for txt in txt_col]
+    entities_bc5cdr_md=[self.extract_entities_spacy(txt,self.nlp_bc5cdr_md)for txt in txt_col]
+    entities_biober=[self.extract_entities_biobert(txt)for txt in txt_col]
+    return entities_sci_sm,entities_bc5cdr_md,entities_biobert
+
+def compare_data
