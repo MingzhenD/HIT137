a07d392ff642dc73c720080cf666184eb25e6bd8
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-12 11:36:32 +0930

    Update q1_task3.1.py

--- q1_task3.1.py
+++ q1_task3.1.py
@@ -1,27 +1,33 @@
-import csv, os
+import os
+import pandas as pd
 from collections import Counter
 
-PATH = os.path.dirname(__file__)
-TEXTFILE = "ALLTEXT.txt"
-CSV = "top30.csv"
-COUNT = 30
-TXT_PATH = os.path.join(PATH, TEXTFILE)
-CSV_PATH = os.path.join(PATH, CSV)
+# Obtain user's directory
+home_dir = os.path.expanduser("~")
 
+# Construct path to txt file in Downloads directory
+txt_file_path = os.path.join(home_dir, "Downloads", "Combined_Text.txt")
 
-def main():
-    counter = Counter()
-    with open(TXT_PATH, "r") as text:
-        for line in text:
-            split_words = line.split()
-            counter.update(Counter(split_words))
+# Read contents of txt file
+with open(txt_file_path, 'r', encoding='utf-8') as txt_file:
+    txt_content = txt_file.read()
 
-    resultDict = dict(counter.most_common(COUNT))
-    with open(CSV_PATH, "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["word", "count"])
-        for key, value in resultDict.items():
-            writer.writerow([key, value])
+# Tokenize the text into words
+words = txt_content.split()
 
+# Record occurrences of each word
+word_counts = Counter(words)
 
-main()
+# Formulate top 30 most common words
+top_30_words = word_counts.most_common(30)
+
+# Create a DataFrame from the top 30 words and corresponding counts
+df_top_30 = pd.DataFrame(top_30_words, columns=['Word', 'Count'])
+
+# Path to the CSV file
+csv_file_path = os.path.join(home_dir, "Downloads", "Top_30_Words_Count.csv")
+
+# Save  DataFrame to CSV file
+df_top_30.to_csv(csv_file_path, index=False)
+
+print("Top 30 common words and their counts exported to:", csv_file_path)
