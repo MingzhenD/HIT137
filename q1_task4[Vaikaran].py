f2471a89a845e5df4dbe14bf02cf9f1d6b5b5f38
Vaikaran <150897497+Vaikaran@users.noreply.github.com>
2024-01-13 17:55:48 +0930

    Update q1_task4[Vaikaran].py

--- q1_task4[Vaikaran].py
+++ q1_task4[Vaikaran].py
@@ -1,5 +1,3 @@
-currently working on this
-
 import spacy
 from spacy import displacy 
 from transformers import BertTokenizer, BertForTokenClassification
@@ -12,27 +10,22 @@ class NERProcessor:
     self.tokenizer=BertTokenizer.from_pretrained(bio_model)
     self.model=BertForTokenClassification.from_pretrained(bio_model)
 
-# Establisng spacy models
     nlp_bc5cdr_md=spacy.load('en_ner_bc5cdr_md')
     nlp_sci_sm=spacy.load('en_core_sci_sm')
 
-# Setting BioBERT pretrained functions
     model=BertForTokenClassification.from_pretrained("monologg/biobert_v1.1_pubmed")
     tokenizer=BertTokenizer.from_pretrained("monologg/biobert_v1.1_pubmed")
 
-# Entity extraction(spacy)
 def extract_entities_spacy(txt,design):
     file=design(txt)
     entities=[(ent.txt,ent.classification) for ent in file.ents]
     return entities
 
-#Entity extraction(BioBERT)
 def extract_entities_biobert(self,txt):
     input=self.tokenizer(txt,return_tensors="pt")
     output=self.model(**input)
     predictions=output.logits.argmax(dim=2)
   
-# Predictions into entities
     entities=[(self.tokenizer.convert_ids_to_tokens(input['input_ids'][0][i].item()),
            self.model.config.id2label[predictions[0][i].item()])
           for i in range(len(predictions[0]))]
